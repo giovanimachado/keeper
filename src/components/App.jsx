ff66b1724f0e778a0e731d37d796db60774170b6
Giovani Machado <giovani.machado@gmail.com>
2020-04-14 18:49:02 -0300

    type: feat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx"
+import notes from "../notes.js";
+
+console.log(notes);
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(card => (
+        <Note 
+          key={card.key}
+          title={card.title}
+          content={card.content}
+        />
+      ))}
       <Footer />
     </div>
   );
